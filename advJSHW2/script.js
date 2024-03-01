8c9f6c98bbd36d4777e7304b6ff63f4ad02cc001
Vladislav Ivanov <jazgigantsmith@gmail.com>
2024-03-01 23:33:06 +0300

    Final commit

---
+++ advJSHW2/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// 1 --------------------------------->
+
+const dropDownElements = document.querySelectorAll('div.dropdown div.menu a.dropdown-item');
+
+dropDownElements.forEach(element => {
+    element.setAttribute('class', element.classList + ' super-dropdown');
+});
+console.log(dropDownElements);
+
+// 2 --------------------------------->
+
+const btnElement = document.querySelector('div.dropdown button.btn');
+
+if (btnElement.classList.contains('btn-secondary')) {
+    btnElement.classList.remove('btn-secondary');
+} else {
+    btnElement.classList.add('btn-secondary');
+}
+
+//  3 --------------------------------->
+
+const menuElement = document.querySelector('div.dropdown div.menu');
+menuElement.classList.remove('dropdown-menu');
+
+//  4 --------------------------------->
+
+const dropDownMainElement = document.querySelector('div.dropdown');
+dropDownMainElement.insertAdjacentHTML('afterbegin', '<a href="#">link</a>');
+
+// 5 --------------------------------->
+
+const elementWithIDDropdownMenuButton = document.getElementById('dropdownMenuButton');
+elementWithIDDropdownMenuButton.id = 'superDropdown';
+
+// 6 --------------------------------->
+
+const elementWithAriaLabDrDowMenBtn = document.querySelector('[aria-labelledby="dropdownMenuButton"]')
+elementWithAriaLabDrDowMenBtn.dataset['dd'] = '3';
+
+// 7 --------------------------------->
+
+document.querySelector('div.dropdown button.dropdown-toggle').removeAttribute('type');
