b1de9a5b607def3d545b744d8a3e6ef6a2068b52
Vladislav Ivanov <jazgigantsmith@gmail.com>
2024-03-18 19:39:18 +0300

    Final seminar to HW6

---
+++ advJSSem5_2Dom/data.js
@@ -0,0 +1,36 @@
+const data = `
+[
+  {
+      "title": "MANGO  PEOPLE  T-SHIRT",
+      "price": "$300",
+      "color": "Red",
+      "size": "Xl",
+      "quantity": "2",
+      "img": "https://flomaster.top/uploads/posts/2023-10/thumbs/1697594917_flomaster-top-p-krasivie-risunki-odezhdi-pinterest-4.jpg"
+  },
+  {
+      "title": "ELLERY X M'O CAPSULE",
+      "price": "$52",
+      "color": "Black",
+      "size": "Xl",
+      "quantity": "1",
+      "img": "https://flomaster.top/uploads/posts/2023-10/thumbs/1697594922_flomaster-top-p-krasivie-risunki-odezhdi-pinterest-8.jpg"
+  },
+  {
+      "title": "BANANA  KIDS  BB-TROUSES",
+      "price": "$320",
+      "color": "Yellow",
+      "size": "S",
+      "quantity": "3",
+      "img": "https://flomaster.top/uploads/posts/2023-10/thumbs/1697594853_flomaster-top-p-krasivie-risunki-odezhdi-pinterest-2.jpg"
+  },
+  {
+      "title": "Z-JACKET CO",
+      "price": "$500",
+      "color": "Blue",
+      "size": "M",
+      "quantity": "2",
+      "img": "https://flomaster.top/uploads/posts/2023-10/1697594889_flomaster-top-p-krasivie-risunki-odezhdi-pinterest-12.jpg"
+  }
+]
+`;
